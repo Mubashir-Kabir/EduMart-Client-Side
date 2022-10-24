ba0cd9ebd221921da9a0c83f55ec4f0ded8750e2
Mubashir Kabir <97.m.kabir@gmail.com>
2022-10-24 23:07:32 +0600

    navbar added

--- src/layout/Root.js
+++ src/layout/Root.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import { Navbar } from "../components/Navbar";
 
 const Root = () => {
   return (
     <div>
-      <h1>this is from root</h1>
+      <Navbar></Navbar>
       <Outlet></Outlet>
     </div>
   );
