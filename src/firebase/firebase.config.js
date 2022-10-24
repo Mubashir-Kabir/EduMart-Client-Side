a47689de60085bb6ab203e95f74835becc864211
Mubashir Kabir <97.m.kabir@gmail.com>
2022-10-24 21:56:03 +0600

    initializing and environment setup

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD3YhXVesclIShs_kYA2psBQ7YX0m_K8ZU",
+  authDomain: "edumart-113ce.firebaseapp.com",
+  projectId: "edumart-113ce",
+  storageBucket: "edumart-113ce.appspot.com",
+  messagingSenderId: "562593053819",
+  appId: "1:562593053819:web:c8dc4d90a6ab6e331776de",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
